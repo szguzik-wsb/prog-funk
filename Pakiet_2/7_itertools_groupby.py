29da6e51e2b87653dc234fb27c7cfc286aff0ab3
x-person <szymon.guzik@gmail.com>
2024-04-08 15:03:52 +0200

    poprawa zadania 7 z pakietu 2

--- Pakiet_2/7_itertools_groupby.py
+++ Pakiet_2/7_itertools_groupby.py
@@ -1,4 +1,11 @@
 from itertools import groupby
+
 liczby = [1, 2, 3, 4, 5, 6, 7, 8]
-zgrupowane_liczby = {k: list(v) for k, v in groupby(liczby, key=lambda x: x % 2)}
-print(zgrupowane_liczby)  # {1: [1, 3, 5, 7], 0: [2, 4, 6, 8]}
+
+# Sortujemy liczby według parzystości, aby `groupby` działało prawidłowo
+liczby_posortowane = sorted(liczby, key=lambda x: x % 2)
+
+# Grupujemy posortowane liczby na podstawie ich parzystości
+zgrupowane_liczby = {k: list(v) for k, v in groupby(liczby_posortowane, key=lambda x: x % 2)}
+
+print(zgrupowane_liczby)
